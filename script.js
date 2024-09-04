043b27702ba36073800a848ac89059a9bf98f386
Andrea Gulli <andreagulli11@gmail.com>
2024-09-04 16:46:04 +0200

    TICKET

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ const { createApp } = Vue;
 // Creo app Vue
 createApp({
     data() {
-        return{
-            
-        }
+        return {
+            images: [
+                {sources: '../img/01.webp', description: 'first image'},
+                {sources: '../img/02.webp', description: 'second image'},
+                {sources: '../img/03.webp', description: 'third image'},
+                {sources: '../img/04.webp', description: 'fourth image'},
+                {sources: '../img/05.webp', description: 'fifth image'}
+            ]
+        };
     }
-})
+}).mount('#app');
